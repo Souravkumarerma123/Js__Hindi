e7e14d951860fa7c2942ed21ad06d33480eda404
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-17 19:47:27 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1); 
+
+/* 
+avoid this type of conversion
+*/
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //false
+
+/* 
+avoid this type of conversion
+*/
+
+// console.log(undefined == 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined > 0); //false
+
+
+// === srtrict check
+
+console.log("2" === 2);
