f8e9003c9067a262e6fe2e2f5fd3bca7d864b93c
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-17 09:21:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+//  "use strict"; //treat all JS code as anewew version
+//  alert(3 + 3) //we are using nodejs,not browser
+ 
+//  console.log(3 + 3); console.log("sourav");
+ /*
+ it is not a good practise,it reduces the code readability
+
+ */
+
+ 
+let name  = "Sourav"
+ let age = 18
+ let isLoggedIn = false
+
+
+ //number => 2 to power 53
+ // big int
+ // string => ""
+ //Boolean => true/false
+ //null => standalone value
+ //undefined => 
+//symbol => unique
+
+
+ //Object
+
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); //object
+ 
+ 
