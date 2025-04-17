f53d8cf59cb6edd7f415685d6ba751bbe12d3e81
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-17 19:22:03 +0000

    conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,15 +15,56 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
  let someNumber = 33
  let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
 
 
 
  //   1=> true; 0=> false
  //    "" => false
  //  "Sourav" => true;
+
+
+//  ++++++++++++++++++++++++++++++++++ OPERATIONS+++++++++++++++++++++
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 +2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+
+ let str1 = "hello"
+ let str2 = " Sourav"
+
+//  console.log(str1 + str2);
  
- 
+// console.log("1" + 2);  //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(+true); //1
+// console.log(+"");  // 0
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++; //ans = 101 value changes after the iteration
+++gameCounter; //ans = 101 value changes before iteration
+console.log(gameCounter);
+
+
+
+
+
+
+
 
   
