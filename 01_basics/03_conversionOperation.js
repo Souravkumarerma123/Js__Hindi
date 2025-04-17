2624f90926814e97d7e1c0ded4678eb6ef2ffb1d
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-17 12:12:50 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Sourav"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//33 => 33
+//"33abc" => NaN
+//true => 1;false => 0
+
+let isLoggedIn = "Sourav"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+
+
+ //   1=> true; 0=> false
+ //    "" => false
+ //  "Sourav" => true;
+ 
+ 
+
+  
