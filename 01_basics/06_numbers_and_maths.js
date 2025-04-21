23e3fb1398d405f6beeb4add917fe9c9cb29ee7c
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-21 20:26:29 +0000

    mat

--- 01_basics/06_numbers_and_maths.js
+++ 01_basics/06_numbers_and_maths.js
@@ -16,7 +16,20 @@ console.log(hundreds.toLocaleString('en-IN'));
 // +++++++++++++++++++++++++++++++ Maths++++++++++++++++++++++++++++++
 
 console.log(Math);
-console.log(Math.abs(-4));
+// console.log(Math.abs(-4)); //abs = absolute
+// console.log(Math.round(4.8)); 
+// console.log(Math.ceil(4.8)); 
+
+// console.log(Math.floor(4.8)); 
+// console.log(Math.min(4,5,6,7,1,23,9)); 
+// console.log(Math.max(4,5,6,7,1,23,9)); 
+// console.log(Math.floor(Math.random()*10) + 1); 
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
 
 
