7982136cd51f797d0c5a0c5e15d71e35ec77dd91
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-21 12:22:35 +0000

    numbers and maths

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.56745
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++ Maths++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+
+
+
