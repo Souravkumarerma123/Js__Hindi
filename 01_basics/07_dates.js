e827855637f3b7971655094ddd7ee7704f22c935
Sourav Kumar <souravkumarverma56@gmail.com>
2025-04-22 18:19:42 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,34 @@
+// Dates
+
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+//  let myCreaatedDate = new Date (2025, 3, 22, 2, 54);
+//  let myCreaatedDate = new Date("2025-01-22");
+// let myCreaatedDate = new Date("01-14-2025");
+// console.log(myCreaatedDate.toDateString());
+//   console.log(myCreaatedDate.toLocaleDateString());
+//  console.log(myCreaatedDate.toLocaleString());
+
+ let myTimeStamp = Date.now()
+
+
+// console.log(myTimeStamp);
+// console.log(myCreaatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate.getDay());
+console.log(newDate.getMo());
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+
+}));
+
+
